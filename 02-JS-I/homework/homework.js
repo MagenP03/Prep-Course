03d662bb5d9dc8efff5e63446102e1ba888cd732
MagenP03 <patersonmateo@gmail.com>
2022-06-17 18:07:39 -0300

    sdasdsadasdaws

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -83,7 +83,7 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
   str1 = 'murcielago'
-  str2 = 'Emurciegalo'
+  str2 = 'murciegalo'
   return str1.lenght === str2.lenght;
 }
 
